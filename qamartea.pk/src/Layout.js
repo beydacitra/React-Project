9b8f993eb0adf6184ca97a63d53bd2c6c5819516
junaid khan <junaidmushtaq988@gmail.com>
2024-07-23 23:24:33 +0500

    Some changes

--- qamartea.pk/src/Layout.js
+++ qamartea.pk/src/Layout.js
@@ -1,15 +1,16 @@
 import { Outlet } from 'react-router-dom';
+
 import Footer from './Components/Footer/Footer';
 import Navbar from './Components/Header/Navbar';
 
 const Layout = () => {
   return (
-    <>
+    <div className='w-screen h-screen bg-black text-white'>
       <Navbar/>
         <Outlet/>
       <Footer/>
-    </>
+    </div>
   )
 }
 
-export default Layout
+export default Layout;
